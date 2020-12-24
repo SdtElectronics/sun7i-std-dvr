2ab2871de3621786e9c072c607848eaff33df478
SdtElectronics <meiyang423@Gmail.com>
2020-12-24 20:00:06 +0800

    add 480x272 lcd support config sources

--- src/boot.cmd
+++ src/boot.cmd
@@ -1,4 +1,6 @@
-setenv bootargs console=ttyS0,115200 console=ttyS7,115200n8 root=/dev/mmcblk0p2 rootwait panic=10
+setenv bootargs console=ttyS0,115200 console=tty0 root=/dev/mmcblk0p2 rootwait panic=10
 load mmc 0:1 0x43000000 sun7i-a20-std-dvr.dtb || load mmc 0:1 0x43000000 boot/sun7i-a20-std-dvr.dtb
+gpio set 232
+gpio clear 232
 load mmc 0:1 0x42000000 zImage || load mmc 0:1 0x42000000 boot/zImage
 bootz 0x42000000 - 0x43000000
